c098c6001b5928d05f0919357c18bc03b3aac78b
SamuelMonsalvesMoreira <samuelmonsalves@gmail.com>
2025-12-06 20:10:42 -0300

    Desafio Héroi DIO

---
+++ index.js
@@ -0,0 +1,32 @@
+let heroi = {
+    nome: "João",
+    idade: 30,
+    tipo: {
+        0: "Guerreiro",
+        1: "Mago",
+        2: "Monge",
+        3: "Ninja"
+    }
+};
+
+let ataque;
+
+atacarHeroi(heroi);
+
+function atacarHeroi(heroi) {
+    for(let index in heroi.tipo) {
+        let classeheroi = heroi.tipo[index]; 
+        
+        if(index == 0) {
+            ataque = "espada";
+        } else if(index == 1) {
+            ataque = "magia";
+        } else if(index == 2) {
+            ataque = "artes marciais";
+        } else if(index == 3) {
+            ataque = "shuriken";
+        }
+
+        console.log(`O ${classeheroi} atacou usando ${ataque}`);
+    }
+}
